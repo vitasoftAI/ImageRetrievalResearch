c94208aa6dacb37184c4c81f32fb4a58a245fb71
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-06 12:16:11 +0900

    square pad script

--- utils/square_pad.py
+++ utils/square_pad.py
@@ -13,11 +13,19 @@ class SquarePad:
   """
 
   def __call__(self, image):
+    
+      # Get width and height of the image
       w, h = image.size
+      
+      # Get max values of the width and height
       max_wh = np.max([w, h])
+      
+      # Create padding
       hp = int((max_wh - w)/2)
       hp_rem = (max_wh - w)%2
       vp = int((max_wh - h)/2)
       vp_rem = (max_wh - h)%2
       padding = (hp, vp, hp+hp_rem, vp+vp_rem)
+      
+      # Apply padding and return the padded image
       return FF.pad(image, padding, 255, 'constant')
