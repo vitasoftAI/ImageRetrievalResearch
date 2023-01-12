dd8b023f1c80ac7f5f5543b62284a6d6dee88a34
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-12 14:43:37 +0900

    Update train_vit_crossentropy.py

--- train/train_vit_crossentropy.py
+++ train/train_vit_crossentropy.py
@@ -244,84 +244,7 @@ def run(args):
 
             return OD([('loss', loss), ('val_top3', top3), ('cos_sims', torch.mean(torch.FloatTensor(cos_sims)))])
 
-        def test_step(self, batch, batch_idx): # triplet loss 
-            
-            """ 
-            Compares test images in the batch with the all images in the dataloader.
-            
-            """
-            fms_ims, fms_poss, fms_negs, scores, cos_sims, gts_all, im_pred_lbls_all, pos_pred_lbls_all = [], [], [], [], [], [], [], []
-            top1, top3, = 0, 0
-            
-            print("\nObtaining embeddings and predicting labels...\n")
-            # Get feature maps and pred labels of the whole test data
-            for i, batch_all in enumerate(test_loader):
-                with torch.no_grad():
-                    
-                    ims_all, poss_all, negs_all, clss_all, regs_all = batch_all['qry'], batch_all['pos'][0], batch_all['neg'][0], batch_all['cat_idx'], batch_all['prod_idx']
-                    out_ims_all = self(ims_all.cuda())
-                    fm_ims_all, lbl_ims_all = out_ims_all[0], out_ims_all[1] # get feature maps [0] and predicted labels [1]
-                    out_poss_all = self(poss_all.cuda())
-                    fm_poss_all, lbl_poss_all = out_poss_all[0], out_poss_all[1] # get feature maps [0] and predicted labels [1]
-                    out_negs_all = self(negs_all.cuda())
-                    fm_negs_all, lbl_negs_all = out_negs_all[0], out_negs_all[1] # get feature maps [0] and predicted labels [1]
-                    fms_ims.extend(fm_ims_all)
-                    fms_poss.extend(fm_poss_all)
-                    fms_negs.extend(fm_negs_all)
-                    gts_all.extend(regs_all.cuda())
-                    im_pred_lbls_all.extend(lbl_ims_all)
-                    pos_pred_lbls_all.extend(lbl_poss_all)
-                
-            fms_ims = torch.stack(fms_ims)
-            fms_poss = torch.stack(fms_poss)
-            fms_negs = torch.stack(fms_negs)
-            gts_all = torch.stack(gts_all)
-            im_pred_lbls_all = torch.stack(im_pred_lbls_all)
-            pos_pred_lbls_all = torch.stack(pos_pred_lbls_all)
-            
-            print("Done!\n")               
-            print("\nCalculating metrics...")
-        
-            # Compute loss, top3, and top1   
-            for index, fm in enumerate(fms_ims):
-                
-                # Compute loss                
-                if only_features and only_labels:
-                    loss_cos_poss = self.cos_loss(fm, fms_poss, labels["pos"].to("cuda")) 
-                    loss_cos_negs = self.cos_loss(fm, fms_negs, labels["neg"].to("cuda"))
-                    loss_cos = loss_cos_poss + loss_cos_negs
-                    loss_ce_ims = self.ce_loss(lbl_ims, regs)
-                    loss_ce_poss = self.ce_loss(lbl_poss, regs)
-                    loss_ce = loss_ce_ims + loss_ce_poss
-                    loss = loss_cos + loss_ce
-                elif only_features == True and only_labels == None:
-                    loss_cos_poss = self.cos_loss(fm, fms_poss, labels["pos"].to("cuda")) 
-                    loss_cos_negs = self.cos_loss(fm, fms_negs, labels["neg"].to("cuda"))
-                    loss_cos = loss_cos_poss + loss_cos_negs
-                    loss = loss_cos                 
-                elif only_features == None and only_labels == True:
-                    loss_ce_ims = self.ce_loss(lbl_ims, regs)
-                    loss_ce_poss = self.ce_loss(lbl_poss, regs)
-                    loss_ce = loss_ce_ims + loss_ce_poss
-                    loss = loss_ce                
-                
-                score_pair = cos(fm.unsqueeze(0), fms_poss[index].unsqueeze(0)) # (1, fm), (len(dl), fm) = (len(dl), fm)
-                scores.append(score_pair)
-                cos_sim = cos(fm.unsqueeze(0), fms_poss)
-                vals, inds = torch.topk(cos_sim, k=3)
-                if gts_all[index] in gts_all[inds]:
-                    top3 += 1
-                if gts_all[index] in gts_all[inds[0]]:
-                    top1 += 1                    
-
-            print("Calculating metrics is done!\n")
-
-            self.log("test_sim_scores", torch.mean(torch.FloatTensor(scores)).item())
-            self.log("test_loss", loss / len(fms_ims))
-            self.log("test_top3", top3 / len(fms_ims))
-            self.log("test_top1", top1 / len(fms_ims))
-
-    def create_model(model_name, conv_input=False, num_classes=num_classes):
+    def create_model(model_name, num_classes = num_classes):
         
         """ 
         
@@ -329,22 +252,14 @@ def run(args):
         
         """
 
+        # Get a model
         if model_name in model_dict:
             base_model = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
-            # base_model.head = Identity()
             print(f"Model {model_name} with the best weights is successfully loaded!")        
-            if conv_input:                
-                conv_layer = Sequential(Conv2d(3, 3, kernel_size=(3, 3), stride=(1, 1),padding=(1,1), bias=False), 
-                 # Conv2d(3, 3, kernel_size=(3, 3), stride=(2, 2),padding=(1,1), bias=False), 
-                 # MaxPool2d(kernel_size=(3,3), stride=(2,2), padding=(1,1)),
-                 SiLU(inplace=True))
-                model = Sequential(conv_layer, base_model)  
-            else:
-                model = base_model
         else:
             assert False, f'Unknown model name "{model_name}". Available models are: {str(model_dict.keys())}'
             
-        return model#.to('cuda')
+        return model
 
     def train_model(model_name, save_name=None, **kwargs):
         
