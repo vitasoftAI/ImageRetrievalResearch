a69591faf71cf85a691142ba6ca238c5b9afad13
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-17 12:19:46 +0900

    Update train_vit_triplet.py

--- train/train_vit_triplet.py
+++ train/train_vit_triplet.py
@@ -173,7 +173,7 @@ def run(args):
             ims, poss, negs, regs = batch['P'], batch['S'], batch['N'], batch['L'] 
             
             # Get predicted labels
-            fm_ims = self.model(ims)
+            lbl_ims = self.model(ims)
             fm_poss = self.model(poss)
             fm_negs = self.model(negs)
             
@@ -181,8 +181,8 @@ def run(args):
             if only_features == True and only_labels == None:
                 
                 # Cosine embedding loss
-                loss_cos_poss = self.cos_loss(fm_ims, fm_poss, labels["pos"].to("cuda")) 
-                loss_cos_negs = self.cos_loss(fm_ims, fm_negs, labels["neg"].to("cuda"))
+                loss_cos_poss = self.cos_loss(lbl_ims, fm_poss, labels["pos"].to("cuda")) 
+                loss_cos_negs = self.cos_loss(lbl_ims, fm_negs, labels["neg"].to("cuda"))
                 loss_cos = loss_cos_poss + loss_cos_negs
                 loss = loss_cos                 
             
@@ -197,16 +197,16 @@ def run(args):
             top3, top1 = 0, 0            
             
             # Go through every feature map
-            for idx, fm in (enumerate(fm_ims)):
+            for idx, fm in (enumerate(lbl_ims)):
                 
                 # Compute cosine similarity of the fm of the query image with it's corresponding positive image feature map
-                sim_pair = cos(fm_ims[idx].unsqueeze(0), fm_poss[idx].unsqueeze(0)) 
+                sim_pair = cos(lbl_ims[idx].unsqueeze(0), fm_poss[idx].unsqueeze(0)) 
                 
                 # Compute cosine similarity of the fm of the query image with it's corresponding negative image feature map
-                unsim_pair = cos(fm_ims[idx].unsqueeze(0), fm_negs[idx].unsqueeze(0)) 
+                unsim_pair = cos(lbl_ims[idx].unsqueeze(0), fm_negs[idx].unsqueeze(0)) 
                 
                 # Compute cosine similarity of the fm of the query image with every feature map in the mini-batch
-                sim = cos(fm_ims[idx].unsqueeze(0), fm_poss) 
+                sim = cos(lbl_ims[idx].unsqueeze(0), fm_poss) 
                 
                 # Add to the list
                 cos_sims.append(sim)
