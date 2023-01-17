0646ef1aaa470db87e6303f4e951af66014a56c9
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-17 12:20:57 +0900

    Update train_vit_triplet.py

--- train/train_vit_triplet.py
+++ train/train_vit_triplet.py
@@ -175,14 +175,14 @@ def run(args):
             # Get predicted labels
             lbl_ims = self.model(ims)
             lbl_poss = self.model(poss)
-            fm_negs = self.model(negs)
+            lbl_negs = self.model(negs)
             
             # Compute loss
             if only_features == True and only_labels == None:
                 
                 # Cosine embedding loss
                 loss_cos_poss = self.cos_loss(lbl_ims, lbl_poss, labels["pos"].to("cuda")) 
-                loss_cos_negs = self.cos_loss(lbl_ims, fm_negs, labels["neg"].to("cuda"))
+                loss_cos_negs = self.cos_loss(lbl_ims, lbl_negs, labels["neg"].to("cuda"))
                 loss_cos = loss_cos_poss + loss_cos_negs
                 loss = loss_cos                 
             
@@ -203,7 +203,7 @@ def run(args):
                 sim_pair = cos(lbl_ims[idx].unsqueeze(0), lbl_poss[idx].unsqueeze(0)) 
                 
                 # Compute cosine similarity of the fm of the query image with it's corresponding negative image feature map
-                unsim_pair = cos(lbl_ims[idx].unsqueeze(0), fm_negs[idx].unsqueeze(0)) 
+                unsim_pair = cos(lbl_ims[idx].unsqueeze(0), lbl_negs[idx].unsqueeze(0)) 
                 
                 # Compute cosine similarity of the fm of the query image with every feature map in the mini-batch
                 sim = cos(lbl_ims[idx].unsqueeze(0), lbl_poss) 
