0bdd3665d93401d8c06e43efccf1e48c10ed1355
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-17 17:37:32 +0900

    Update train_vit_triplet.py

--- train/train_vit_triplet.py
+++ train/train_vit_triplet.py
@@ -266,6 +266,8 @@ def run(args):
                 
                 # Compute cosine similarity of the fm of the query image with it's corresponding negative image feature map
                 unsim_pair = cos(lbl_ims[idx].unsqueeze(0), lbl_negs[idx].unsqueeze(0)) 
+                
+                # Compute cosine similarity of the fm of the query image with every feature map in the mini-batch
                 sim = cos(lbl_ims[idx].unsqueeze(0), lbl_poss) 
                 cos_sims_pair.append(sim_pair)
                 cos_unsims_pair.append(unsim_pair)
