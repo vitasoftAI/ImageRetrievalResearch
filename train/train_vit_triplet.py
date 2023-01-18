2e057c79a75709f4ef215fc98955922cf79bc57d
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-18 12:19:08 +0900

    Update train_vit_triplet.py

--- train/train_vit_triplet.py
+++ train/train_vit_triplet.py
@@ -262,13 +262,13 @@ def run(args):
             for idx, fm in enumerate(fm_ims):
                 
                 # Compute cosine similarity of the fm of the query image with it's corresponding positive image feature map
-                sim_pair = cos(lbl_ims[idx].unsqueeze(0), lbl_poss[idx].unsqueeze(0)) 
+                sim_pair = cos(fm_ims[idx].unsqueeze(0), lbl_poss[idx].unsqueeze(0)) 
                 
                 # Compute cosine similarity of the fm of the query image with it's corresponding negative image feature map
-                unsim_pair = cos(lbl_ims[idx].unsqueeze(0), lbl_negs[idx].unsqueeze(0)) 
+                unsim_pair = cos(fm_ims[idx].unsqueeze(0), lbl_negs[idx].unsqueeze(0)) 
                 
                 # Compute cosine similarity of the fm of the query image with every feature map in the mini-batch
-                sim = cos(lbl_ims[idx].unsqueeze(0), lbl_poss) 
+                sim = cos(fm_ims[idx].unsqueeze(0), lbl_poss) 
                 
                 # Add to the cos_similarities list
                 cos_sims_pair.append(sim_pair)
@@ -289,8 +289,8 @@ def run(args):
             self.log("val_loss", loss)
             self.log("cos_sims", torch.mean(torch.FloatTensor(cos_sims_pair)).item())
             self.log("cos_unsims", torch.mean(torch.FloatTensor(cos_unsims_pair)).item())
-            self.log("val_top3", top3 / len(lbl_ims))
-            self.log("val_top1", top1 / len(lbl_ims))
+            self.log("val_top3", top3 / len(fm_ims))
+            self.log("val_top1", top1 / len(fm_ims))
 
             return OD([('loss', loss), ('val_top3', top3), ('cos_sims', torch.mean(torch.FloatTensor(cos_sims)))])
 
