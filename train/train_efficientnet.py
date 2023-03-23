4f7093e187896524722f1127c7fb4ade2e5f84d8
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-23 11:42:24 +0900

    Update train_efficientnet.py

--- train/train_efficientnet.py
+++ train/train_efficientnet.py
@@ -417,7 +417,17 @@ def run(args):
         
         """ 
         
-        Gets model name and creates a timm model.
+        This function gets model name and creates a timm model.
+        
+        Arguments:
+        
+            model_name  - name of the model in timm library, str;
+            conv_input  - option for the the input to pass convolution layer first, bool;
+            num_classes - number of classes in the dataset.
+            
+        Output:
+        
+            model       - created model from timm library.
         
         """
 
