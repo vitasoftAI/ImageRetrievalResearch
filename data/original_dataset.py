8c460a331040feb6af4a270fc5e133a87c8f6063
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-04-05 17:17:12 +0900

    Update original_dataset.py

--- data/original_dataset.py
+++ data/original_dataset.py
@@ -1,13 +1,8 @@
 # Import libraries
 from torch.utils.data import Dataset
-import glob
-import os
+import glob, os, random, json, tqdm, csv
 from PIL import Image
-import random
 import numpy as np
-import json
-import tqdm
-import csv
 
 # Data split function
 def data_split(data_dir, out_path, policy: str = 'prod', hard_split: bool = True, train_essentials: str = '', split: list = [0.8, 0.1, 0.1]):
@@ -17,14 +12,15 @@ def data_split(data_dir, out_path, policy: str = 'prod', hard_split: bool = True
     
     Arguments:
     
-        data_dir - directory with the data;
-        out_path - path to output the json file with data split;
-        policy - policy to split data;
-        hard_split - if True, not all classes in the validation and test sets, else train, val, test sets have samples for all classes;
-        train_essentials - csv file with class names for train;
-        split - data split ratio.
+        data_dir            - directory with the data, str;
+        out_path            - path to output the json file with data split, str;
+        policy              - policy to split data, str;
+        hard_split          - if True, not all classes in the validation and test sets, else train, val, test sets have samples for all classes, bool;
+        train_essentials    - csv file with class names for train, str;
+        split               - data split ratio, list.
     
     """    
+    
     assert sum(split) == 1, 'sum of split should be 1'
     
     # Initialize train essentials list
